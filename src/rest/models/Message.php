ac7892d94247e6daad7e79f6a405f1c6776d3a51
xutongle <xutongle@gmail.com>
2018-04-10 16:28:05 +0800

    update

--- src/rest/models/Message.php
+++ src/rest/models/Message.php
@@ -7,6 +7,8 @@
 
 namespace yuncms\message\rest\models;
 
+use yuncms\rest\models\User;
+
 /**
  * Class Message
  *
@@ -15,5 +17,36 @@ namespace yuncms\message\rest\models;
  */
 class Message extends \yuncms\message\models\Message
 {
+    /**
+     * @return array
+     */
+    public function fields()
+    {
+        return [
+            'id',
+            'parent',
+            'status',
+            'message',
+            'user',
+            'from',
+            'created_at',
+            'updated_at',
+        ];
+    }
 
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFrom()
+    {
+        return $this->hasOne(User::class, ['id' => 'from_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
 }
